86f522589b1b7f4108037e3559d4c74e90801de4
FlozyTess <cecilrency23@gmail.com>
2024-12-13 00:38:42 +0300

    defined the array and added destructive functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution here!
+ //define the initial cats array
+ let cats = ["Milo","Otis", "Garfield"];
+//destructive functions
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
